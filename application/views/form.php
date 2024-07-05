9e45f3cc8e9b3fbfdc81198a90769bf6c911671b
faklarion <faisal.hazard17@gmail.com>
2024-07-05 21:48:31 +0800

    test 2

--- application/views/form.php
+++ application/views/form.php
@@ -15,7 +15,6 @@
                             <tr><td>Datalist</td><td><?php echo datalist_dinamis('test', 'tbl_user', 'full_name') ?></td></tr>
                             <tr><td>Combobox</td><td><?php echo cmb_dinamis('test', 'tbl_user', 'full_name', 'id_users') ?></td></tr>
                         </table>
-
                     </div>
                 </div>
             </div>
